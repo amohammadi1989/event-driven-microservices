36e9bed184216862a3037e642bfc21ec7b8e8905
amohammadi1989 <clone.1989@gmail.com>
2022-03-06 13:52:32 +0330

    added batch-to-kafka-services

---
+++ batch-to-kafka-services/src/main/java/com/example/batchtokafka/BatchToKafkaServicesApplication.java
@@ -0,0 +1,13 @@
+package com.example.batchtokafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchToKafkaServicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BatchToKafkaServicesApplication.class, args);
+    }
+
+}
---
+++ batch-to-kafka-services/src/test/java/com/example/batchtokafka/BatchToKafkaServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.batchtokafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchToKafkaServicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
